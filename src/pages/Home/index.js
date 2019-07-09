b6cdde20a92ff0552ade4f36581db24801432323
José Castor <junior.castor@gmail.com>
2019-07-09 16:57:06 -0300

    Styling page home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,92 @@
 import React, { Component } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
 // import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default class Home extends Component {
   state = {};
 
   render() {
-    return <h1>Home</h1>;
+    return (
+      <ProductList>
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-under-armour-micro-g-aurora-masculino/58/B78-1713-058/B78-1713-058_zoom2.jpg"
+            alt="Tênis"
+          />
+          <strong>Tênis massa</strong>
+          <span>R$ 129,90</span>
+
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-under-armour-micro-g-aurora-masculino/58/B78-1713-058/B78-1713-058_zoom2.jpg"
+            alt="Tênis"
+          />
+          <strong>Tênis massa</strong>
+          <span>R$ 129,90</span>
+
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-under-armour-micro-g-aurora-masculino/58/B78-1713-058/B78-1713-058_zoom2.jpg"
+            alt="Tênis"
+          />
+          <strong>Tênis massa</strong>
+          <span>R$ 129,90</span>
+
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-under-armour-micro-g-aurora-masculino/58/B78-1713-058/B78-1713-058_zoom2.jpg"
+            alt="Tênis"
+          />
+          <strong>Tênis massa</strong>
+          <span>R$ 129,90</span>
+
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-under-armour-micro-g-aurora-masculino/58/B78-1713-058/B78-1713-058_zoom2.jpg"
+            alt="Tênis"
+          />
+          <strong>Tênis massa</strong>
+          <span>R$ 129,90</span>
+
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      </ProductList>
+    );
   }
 }
