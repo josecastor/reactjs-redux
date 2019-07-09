1d6344f1f0391c59ba28a46227126a92a37fed54
José Castor <junior.castor@gmail.com>
2019-07-09 19:26:46 -0300

    Get products of api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,91 +1,47 @@
 import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+
+import api from '../../services/api';
 
 // import { View } from 'react-native';
 
 import { ProductList } from './styles';
 
 export default class Home extends Component {
-  state = {};
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
 
   render() {
+    const { products } = this.state;
     return (
       <ProductList>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-under-armour-micro-g-aurora-masculino/58/B78-1713-058/B78-1713-058_zoom2.jpg"
-            alt="Tênis"
-          />
-          <strong>Tênis massa</strong>
-          <span>R$ 129,90</span>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
 
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 3
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-under-armour-micro-g-aurora-masculino/58/B78-1713-058/B78-1713-058_zoom2.jpg"
-            alt="Tênis"
-          />
-          <strong>Tênis massa</strong>
-          <span>R$ 129,90</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 3
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-under-armour-micro-g-aurora-masculino/58/B78-1713-058/B78-1713-058_zoom2.jpg"
-            alt="Tênis"
-          />
-          <strong>Tênis massa</strong>
-          <span>R$ 129,90</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 3
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-under-armour-micro-g-aurora-masculino/58/B78-1713-058/B78-1713-058_zoom2.jpg"
-            alt="Tênis"
-          />
-          <strong>Tênis massa</strong>
-          <span>R$ 129,90</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 3
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-under-armour-micro-g-aurora-masculino/58/B78-1713-058/B78-1713-058_zoom2.jpg"
-            alt="Tênis"
-          />
-          <strong>Tênis massa</strong>
-          <span>R$ 129,90</span>
-
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 3
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
       </ProductList>
     );
   }
