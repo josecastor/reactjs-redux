dc3b56600a9947eaf4604cc2192de10f30202fec
José Castor <junior.castor@gmail.com>
2019-07-09 14:22:27 -0300

    Global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyles from './styles/global';
+
 import Route from './routes';
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       {/** <Header /> */}
       <Route />
     </BrowserRouter>
